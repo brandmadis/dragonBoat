68eb273d6c94c26036d79f5192075cef86f37a0f
brandmadis <brandmadis@outlook.com>
2019-04-25 06:35:05 -0700

    Add performance testing

--- src/components/home.js
+++ src/components/home.js
@@ -1,18 +1,29 @@
-import React from 'react' 
+import React, { Component } from 'react' 
 import { config } from '../firebase'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-const Home = () => {
-    let key = process.env.REACT_APP_devApiKey
-    let test = "test"
-    return (
-        <div>
+class Home extends Component {
+    state = {
+        t0: 0
+    }
+  
+    componentDidMount(){
+
+        let t1 = performance.now()
+        console.log(`/home Mount time: ${t1-this.state.t0}`)
+    }
+    render(){
+
+        let key = process.env.REACT_APP_devApiKey
+        let test = "test"
+        return (
+            <div>
             <p>Home</p>
-            <FontAwesomeIcon icon="stroopwafel" />  
 
         </div>
             
-    )
+            )
+        }
 } 
 
 export default Home
